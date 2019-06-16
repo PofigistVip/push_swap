a43e68284b367878675d85f6409293a6704721ee
PofigistVip <pruskalev@gmail.com>
2019-06-16 09:09:25 +0300

    changes for ft_common.h

--- include/ft_checker.h
+++ include/ft_checker.h
@@ -1,18 +1,9 @@
 #ifndef FT_CHECKER_H
 # define FT_CHECKER_H
+# include "ft_common.h"
 # include "ft_stack.h"
 # include "ft_checker.h"
 
-typedef struct	s_checker_options
-{
-	int		is_checker;
-	int		argc;
-	char	**arguments;
-	int		debug;
-}				t_checker_options;
-
-void	ft_splited_free(char **splited);
-int		ft_get_args(t_stack *a, t_checker_options *opt);
 int		ft_read_and_do_instructions(t_stack *a, t_stack *b,
 			t_checker_options *opt);
 int		ft_show_debug(t_stack *a, t_stack *b, char *instruction, int first);
