152a0b37e05f3c2dc31c181deea38f09f760d609
PofigistVip <pruskalev@gmail.com>
2019-06-16 09:08:12 +0300

    some transfer to common

---
+++ include/ft_common.h
@@ -0,0 +1,17 @@
+
+#ifndef FT_COMMON_H
+# define FT_COMMON_H
+# include "ft_stack.h"
+
+typedef struct	s_checker_options
+{
+	int		is_checker;
+	int		argc;
+	char	**arguments;
+	int		debug;
+}				t_checker_options;
+
+void	ft_splited_free(char **splited);
+int		ft_get_args(t_stack *a, t_checker_options *opt);
+
+#endif
